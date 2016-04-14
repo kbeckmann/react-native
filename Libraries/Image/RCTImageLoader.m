93f38ba23bef29ea9bad39eff40563ec922739e3
Konrad Beckmann <konrad.beckmann@mindoktor.se>
2016-04-14 16:57:03 +0200

    Expose ImageLoader.flushCache() to flush the image cache from js

--- Libraries/Image/RCTImageLoader.m
+++ Libraries/Image/RCTImageLoader.m
@@ -635,6 +635,12 @@ static UIImage *RCTResizeImageIfNeeded(UIImage *image,
   }
 }
 
+RCT_EXPORT_METHOD(flushCache:(NSInteger)foo)
+{
+  [_URLCache removeAllCachedResponses];
+}
+
+
 @end
 
 @implementation RCTBridge (RCTImageLoader)
